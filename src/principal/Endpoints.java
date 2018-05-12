52d0a20918d417a151c5dc857f98093d42d0dc50
Edward Gómez <edward_25nov@hotmail.com>
2018-05-12 09:52:07 -0600

    add findElements

--- src/principal/Endpoints.java
+++ src/principal/Endpoints.java
@@ -7,5 +7,10 @@ public class Endpoints {
 	public static String desarrollo ="";
 	public static String qa ="";
 	public static String url1 = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
-
+	public static String byName = "http://testing.todvachev.com/selectors/name/";
+	public static String byId = "http://testing.todvachev.com/selectors/id/";
+	public static String byXpath = "http://testing.todvachev.com/selectors/xpath/";
+	public static String byCss = "http://testing.todvachev.com/selectors/css-path/";
+	public static String byClass = "http://testing.todvachev.com/selectors/class-name/";
+	
 }
--- src/principal/Training.java
+++ src/principal/Training.java
@@ -3,7 +3,9 @@
  */
 package principal;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 /**
  * @author Edward
@@ -16,14 +18,120 @@ public class Training {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		findbyClass();
+	}
+	
+	public static void findbyName() 
+	{
 		WebDriver _driver = WebDriverCreator.getChromeDriver();
 		/// sirve para ir a una url en especifico
-		_driver.navigate().to(Endpoints.url1);
+		_driver.navigate().to(Endpoints.byName);
 		
+		// buscar el elemento inpunt por name
+		WebElement input =    _driver.findElement(By.name("myName"));
+		/// con la instrucción sendKeys, se puede mandar a escribir texto
+		input.sendKeys("Hola BDG!!");
 		/// cerrar el driver
+		try {
+			Thread.sleep(2000); // importante .. 
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		_driver.close();
+		_driver.quit();
+		
+	}
+	
+	public static void findbyId() 
+	{
+		WebDriver _driver = WebDriverCreator.getChromeDriver();
+		/// sirve para ir a una url en especifico
+		_driver.navigate().to(Endpoints.byId);
+		
+		// buscar el elemento inpunt por name
+		WebElement image =    _driver.findElement(By.id("testImadfaage"));
+	
+		/// cerrar el driver
+		try {
+			Thread.sleep(2000); // importante .. 
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		_driver.close();
+		_driver.quit();
+		
+	}
+	
+
+	public static void findbyXpath() 
+	{
+		WebDriver _driver = WebDriverCreator.getChromeDriver();
+		/// sirve para ir a una url en especifico
+		_driver.navigate().to(Endpoints.byXpath);
+		
+		// buscar el elemento inpunt por name
+		WebElement image =    _driver.findElement(By.xpath("//*[@id=\"post-109\"]/div/figure/img"));
+	
+		/// cerrar el driver
+		try {
+			Thread.sleep(2000); // importante .. 
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		_driver.close();
+		_driver.quit();
+		
+	}
+	
+	public static void findbyCss() 
+	{
+		WebDriver _driver = WebDriverCreator.getChromeDriver();
+		/// sirve para ir a una url en especifico
+		_driver.navigate().to(Endpoints.byCss);
+		
+		// buscar el elemento inpunt por name
+		WebElement image =    _driver.findElement(By.cssSelector("#post-108 > div > figure > img"));
+	
+		/// cerrar el driver
+		try {
+			Thread.sleep(2000); // importante .. 
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		_driver.close();
+		_driver.quit();
+		
+	}
+	
+	
+	public static void findbyClass() 
+	{
+		
+		WebDriver _driver = WebDriverCreator.getChromeDriver();
+		/// sirve para ir a una url en especifico
+		_driver.navigate().to(Endpoints.byClass);
+		
+		// buscar el elemento inpunt por name
+		WebElement image =    _driver.findElement(By.className("testClass"));
+	    System.out.println("TEXT VALUE: "+image.getText());
+		/// cerrar el driver
+		try {
+			Thread.sleep(2000); // importante .. 
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		_driver.close();
 		_driver.quit();
 	}
-
+	
 }
